8dd23007dfe10364548d900508af3ae365ef0988
Simon Tucker <simon@brotherlogic@gmail>
2019-01-01 23:21:39 -0800

    Saves lists on add.

--- wantslistutils_test.go
+++ wantslistutils_test.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"testing"
+
+	"github.com/brotherlogic/keystore/client"
 )
 
 func InitTestServer() *Server {
 	s := Init()
 	s.SkipLog = true
+	s.GoServer.KSclient = *keystoreclient.GetTestClient(".test")
 	return s
 }
 
