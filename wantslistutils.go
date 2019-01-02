aa2864fb927531f3f3580bdcb778b42ed86ff310
Simon Tucker <simon@brotherlogic@gmail>
2019-01-01 23:47:40 -0800

    Added logic to cover move. This closes #14.

--- wantslistutils.go
+++ wantslistutils.go
@@ -1,4 +1,38 @@
 package main
 
-func blank() {
+import (
+	"fmt"
+	"sort"
+
+	"golang.org/x/net/context"
+
+	pb "github.com/brotherlogic/wantslist/proto"
+)
+
+func (s *Server) processWantLists(ctx context.Context) {
+	for _, list := range s.config.Lists {
+		sort.SliceStable(list.Wants, func(i, j int) bool {
+			return list.Wants[i].Index < list.Wants[j].Index
+		})
+
+		var toUpdateToWanted *pb.WantListEntry
+		if list.Wants[0].Status == pb.WantListEntry_UNPROCESSED {
+			toUpdateToWanted = list.Wants[0]
+		} else {
+			for i := range list.Wants[1:] {
+				if list.Wants[i].Status == pb.WantListEntry_COMPLETE && list.Wants[i+1].Status == pb.WantListEntry_UNPROCESSED {
+					toUpdateToWanted = list.Wants[i+1]
+				}
+			}
+		}
+
+		if toUpdateToWanted != nil {
+			err := s.wantBridge.want(ctx, toUpdateToWanted.Want)
+			s.Log(fmt.Sprintf("Updating %v to WANTED with error %v", toUpdateToWanted.Want, err))
+			if err == nil {
+				toUpdateToWanted.Status = pb.WantListEntry_WANTED
+			}
+
+		}
+	}
 }
--- wantslistutils_test.go
+++ wantslistutils_test.go
@@ -1,18 +1,75 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/brotherlogic/keystore/client"
+	pb "github.com/brotherlogic/wantslist/proto"
+	"golang.org/x/net/context"
 )
+
+type testWantBridge struct {
+	fail bool
+}
+
+func (t *testWantBridge) want(ctx context.Context, id int32) error {
+	if t.fail {
+		return fmt.Errorf("Built to fail")
+	}
+	return nil
+}
 
 func InitTestServer() *Server {
 	s := Init()
 	s.SkipLog = true
 	s.GoServer.KSclient = *keystoreclient.GetTestClient(".test")
+	s.wantBridge = &testWantBridge{}
 	return s
 }
 
-func TestFirst(t *testing.T) {
-	blank()
+func TestFirstEntrySet(t *testing.T) {
+	s := InitTestServer()
+	s.AddWantList(context.Background(), &pb.AddWantListRequest{
+		Add: &pb.WantList{
+			Name: "TestList",
+			Wants: []*pb.WantListEntry{
+				&pb.WantListEntry{Index: 1, Want: 123},
+				&pb.WantListEntry{Index: 2, Want: 125},
+			},
+		},
+	})
+
+	s.processWantLists(context.Background())
+
+	lists, err := s.GetWantList(context.Background(), &pb.GetWantListRequest{})
+	if err != nil {
+		t.Fatalf("Error getting wants: %v", err)
+	}
+	if lists.Lists[0].Wants[0].Status != pb.WantListEntry_WANTED {
+		t.Errorf("Want has not been updated")
+	}
+}
+
+func TestFirstEntryUpdated(t *testing.T) {
+	s := InitTestServer()
+	s.AddWantList(context.Background(), &pb.AddWantListRequest{
+		Add: &pb.WantList{
+			Name: "TestList",
+			Wants: []*pb.WantListEntry{
+				&pb.WantListEntry{Index: 1, Want: 123, Status: pb.WantListEntry_COMPLETE},
+				&pb.WantListEntry{Index: 2, Want: 125},
+			},
+		},
+	})
+
+	s.processWantLists(context.Background())
+
+	lists, err := s.GetWantList(context.Background(), &pb.GetWantListRequest{})
+	if err != nil {
+		t.Fatalf("Error getting wants: %v", err)
+	}
+	if lists.Lists[0].Wants[1].Status != pb.WantListEntry_WANTED {
+		t.Errorf("Want has not been updated following first complete")
+	}
 }
