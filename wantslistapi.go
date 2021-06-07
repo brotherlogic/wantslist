9ed0bcd3c6ddc4e89030341d58e0a91cfb219217
Simon Tucker <brotherlogic@gmail.com>
2021-06-06 18:16:37 -0700

    Bounce

--- wantslistapi.go
+++ wantslistapi.go
@@ -16,7 +16,12 @@ func (s *Server) AddWantList(ctx context.Context, req *pb.AddWantListRequest) (*
 	if err != nil {
 		return nil, err
 	}
+
 	if len(config.Lists) != 5 {
+		s.RaiseIssue("Moar Wants", "You need to add some wants lists")
+	}
+
+	if len(config.Lists) > 5 {
 		return nil, fmt.Errorf("You need to have 5 lists - you have %v", len(config.Lists))
 	}
 
