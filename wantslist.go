011740323525c67c2643111ed10926d4cb15a075
Simon Tucker <brotherlogic@gmail.com>
2021-06-08 06:14:49 -0700

    Adjust number to 3

--- wantslist.go
+++ wantslist.go
@@ -152,7 +152,7 @@ func (s *Server) load(ctx context.Context) (*pb.Config, error) {
 	}
 	config.Lists = lists
 
-	if len(config.Lists) != 5 {
+	if len(config.Lists) != 3 {
 		s.RaiseIssue("Wrong wants", fmt.Sprintf("You need to add/remove some wants lists: %v is how many you have", len(config.Lists)))
 	}
 
